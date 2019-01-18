3cbce0160a0072a3f14c1cfac17f80ff5d57f332
walterpark <neodust223@gmail.com>
2019-01-18 18:51:05 +0900

    매개변수 사용하는 함수 추가

--- sqrt.py
+++ sqrt.py
@@ -15,3 +15,12 @@ b = -6
 c = -8
 
 print_root()
+
+
+def print_root2(a, b, c):
+    r1 = (-b + (b**2 - 4 * a * c) ** 0.5) / (2 * a)
+    r2 = (-b - (b**2 - 4 * a * c) ** 0.5) / (2 * a)
+
+    print('해는 {} 또는 {}'.format(r1,r2))
+
+print_root2(3,4,5)
