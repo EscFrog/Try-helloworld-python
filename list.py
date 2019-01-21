35006672875f016d115174742270093b9d06ca61
walterpark <neodust223@gmail.com>
2019-01-21 12:25:40 +0900

    list 연습 파일 커밋

---
+++ list.py
@@ -0,0 +1,13 @@
+list1 = ['가위', '바위', '보']
+list2 = [37, 23, 10, 33, 29, 40]
+
+print(list1)
+print(list2)
+
+print(list1[0])
+print(list2[1])
+
+print(list2[-6])
+print(list2[-7]) # 리스트보다 큰 값을 넣었으므로 오류가 남
+
+
